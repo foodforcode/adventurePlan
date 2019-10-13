2756c6e2885893adf646820dc118f71d3164d00a
Connie T <tsai.connie@yahoo.com>
2019-10-13 01:44:47 -0700

    Add styling

--- advPlan.js
+++ advPlan.js
@@ -1,12 +1,27 @@
 var destItem = $("#dest").val();
 var length = $("#length").val();
-var boxes = $(".box")
-// function postDisplay {
-// 	$(this).text() === destItem ? change = destItem : change = length;
-// 	$(this).text(change);
-// 	$(this).prop("disabled", "true");
-// 	postDisplay();
-// }
+var boxes = [];
+
+
+$("#dest").keydown(function(event) {
+	if(event.which === 9 || event.which === 13) {
+		$(".display1").css("display", "block");
+		$("#destinationDisplay").text(' ' + destItem);
+		$(this).text(destItem);
+		$(this).prop("disabled", "true");
+	}
+});
+
+$("#length").keydown(function(event){
+	if(event.which === 13) {
+		$("#daysDisplay").text(' ' + length);
+		$(".display2").css("display", "block");
+		$(this).text(length);
+		$(this).prop("disabled", "true");
+	}
+});
+//how to loop through and only append on ul input is entered
+for(var i = 0; i < boxes.length; i++) {
 
 $(".listItem").keypress(function(event) {
 	if(event.which === 13) {
@@ -14,9 +29,20 @@ $(".listItem").keypress(function(event) {
 		var todoItem = $(this).val();
 		$(this).val("");
 		//create new li and add to ul
-		$(this).append('<li><span><i class="fas fa-trash-alt"></i></span> ' + todoItem + '</li>');
-	}
+		$("ul").append('<li><span><i class="fas fa-trash-alt"></i></span> ' + todoItem + '</li>');
+	};
 });
+}
+
+// $(".listItem").keypress(function(event) {
+// 	if(event.which === 13) {
+// 		//store todo from input
+// 		var todoItem = $(this).val();
+// 		$(this).val("");
+// 		//create new li and add to ul
+// 		$("ul").append('<li><span><i class="fas fa-trash-alt"></i></span> ' + todoItem + '</li>');
+// 	};
+// });
 
 //check off lis by clicking
 $("ul").on("click", "li", function(){
@@ -30,24 +56,4 @@ $("ul").on("click", "span", function(event) {
 		$(this).remove();
 	});
 	event.stopPropagation();
-});
-
-$("#dest").keydown(function(event) {
-	if(event.which === 9 || event.which === 13) {
-		var destItem = $(this).val();
-		$("#destinationDisplay").text(' ' + destItem);
-		$(".display1").css("display", "block");
-		$(this).text(destItem);
-		$(this).prop("disabled", "true");
-	}
-});
-
-$("#length").keydown(function(event){
-	if(event.which === 13) {
-		var length = $("#length").val();
-		$("#daysDisplay").text(' ' + length);
-		$(".display2").css("display", "block");
-		$(this).text(length);
-		$(this).prop("disabled", "true");
-	}
 });
