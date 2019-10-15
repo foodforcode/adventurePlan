388c6efe412f732419ffd92b97a16f613c56e6ad
Connie T <tsai.connie@yahoo.com>
2019-10-14 18:59:46 -0700

    Final round
    styling and glyphicons added

--- advPlan.js
+++ advPlan.js
@@ -1,57 +1,56 @@
-var destItem = $("#dest").val();
-var length = $("#length").val();
-var boxes = [];
+// $("#dest").keydown(function(event) {
+// 	if(event.which === 9 || event.which === 13) {
+//     var destItem = $("#dest").val();
+// 		$(".display1").css("display", "block");
+// 		$("#destinationDisplay").text(' ' + destItem);
+// 		$(this).text(destItem);
+// 	}
+// });
 
+// $("#length").keydown(function(event){
+// 	if(event.which === 13 || event.which === 9) {
+//     var length = $("#length").val();
+// 		$("#daysDisplay").text(' ' + length);
+// 		$(".display2").css("display", "block");
+// 		$(this).text(length);
+// 	};
+// }); 
 
-$("#dest").keydown(function(event) {
-	if(event.which === 9 || event.which === 13) {
-		$(".display1").css("display", "block");
-		$("#destinationDisplay").text(' ' + destItem);
-		$(this).text(destItem);
-		$(this).prop("disabled", "true");
-	}
-});
-
-$("#length").keydown(function(event){
-	if(event.which === 13) {
-		$("#daysDisplay").text(' ' + length);
+$("button").on("click", function() {
+    var length = $("#length").val();
+    var destItem = $("#dest").val();
+    $(".display1").css("display", "block");
 		$(".display2").css("display", "block");
-		$(this).text(length);
-		$(this).prop("disabled", "true");
-	}
-});
-//how to loop through and only append on ul input is entered
-for(var i = 0; i < boxes.length; i++) {
+		$("#destinationDisplay").text(' ' + destItem);
+    $("#daysDisplay").text(' ' + length);
+		$("#destination").text(destItem);
+    $("#length").text(length);
+    $("#destination").prop("disabled", "true");
+    $("#length").prop("disabled", "true");
+    alert("Keep track of travel plans and check them off as you go. Happy travels!")
+	});
 
-$(".listItem").keypress(function(event) {
+$(".listItem").keydown(function(event) {
 	if(event.which === 13) {
 		//store todo from input
 		var todoItem = $(this).val();
 		$(this).val("");
 		//create new li and add to ul
-		$("ul").append('<li><span><i class="fas fa-trash-alt"></i></span> ' + todoItem + '</li>');
+		populate($(this).attr('name'), todoItem);
 	};
 });
-}
 
-// $(".listItem").keypress(function(event) {
-// 	if(event.which === 13) {
-// 		//store todo from input
-// 		var todoItem = $(this).val();
-// 		$(this).val("");
-// 		//create new li and add to ul
-// 		$("ul").append('<li><span><i class="fas fa-trash-alt"></i></span> ' + todoItem + '</li>');
-// 	};
-// });
+function populate(target, todoItem) {
+  $("#" + target).append('<li><span class="trash"><i class="fas fa-trash-alt"></i></span> ' + todoItem + '<span class="check"><i class="fas fa-check"></i></span>' + '</li>');
+};
 
 //check off lis by clicking
-$("ul").on("click", "li", function(){
-	//if li is gray turn black, else turn gray
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
 });
 
 //click on X to delete li
-$("ul").on("click", "span", function(event) {
+$("ul").on("click", ".trash", function(event) {
 	$(this).parent().fadeOut(450, function(){
 		$(this).remove();
 	});
